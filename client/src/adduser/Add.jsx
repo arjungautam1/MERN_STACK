b1c2ab899b788ff12dcf35e052353c3d3fffdecd
arjungautam1 <arjungautam5431@gmail.com>
2024-03-18 19:12:04 +0545

    add user form

--- client/src/adduser/Add.jsx
+++ client/src/adduser/Add.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import "./add.css";
+import { Link } from "react-router-dom";
 
 const Add = () => {
   return (
-    <div>
-      <h1>Add User</h1>
-      <div>AddUser Table</div>
+    <div className="addUser">
+      <Link to="/" type="button" className="btn btn-secondary">
+        <i className="fa-solid fa-backward"></i> Back
+      </Link>
+
+      <h3>Add New User</h3>
+      <form className="addUserForm">
+        <div className="inputGroup">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            autoComplete="off"
+            placeholder="Enter your Name"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="off"
+            placeholder="Enter your Email"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="address">Address:</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            autoComplete="off"
+            placeholder="Enter your Address"
+          />
+        </div>
+        <div className="inputGroup">
+          <button type="button" class="btn btn-primary mt-4 p-2">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
