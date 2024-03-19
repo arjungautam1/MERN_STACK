a58ccb6dc7f555d2e7633c49c4a86f65a33cb385
arjungautam1 <arjungautam5431@gmail.com>
2024-03-19 22:26:33 +0545

    demo

--- client/src/getuser/User.jsx
+++ client/src/getuser/User.jsx
@@ -36,45 +36,52 @@ const User = () => {
         Add User <i className="fa-solid fa-user-plus"></i>
       </Link>
 
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">S.No.</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Address</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => {
-            return (
-              <tr key={user._id}>
-                <td>{index + 1}</td>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.address}</td>
-                <td className="actionButtons">
-                  <Link
-                    to={`/update/` + user._id}
-                    type="button"
-                    className="btn btn-info"
-                  >
-                    <i className="fa-solid fa-pen-to-square"></i>
-                  </Link>
-                  <button
-                    onClick={() => deleteUser(user._id)}
-                    type="button"
-                    className="btn btn-danger"
-                  >
-                    <i className="fa-solid fa-trash"></i>
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {users.length === 0 ? (
+        <div className="noData">
+          <h3>No Data To Display</h3>
+          <p>Please Add User.</p>
+        </div>
+      ) : (
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">S.No.</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Address</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => {
+              return (
+                <tr key={user._id}>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.address}</td>
+                  <td className="actionButtons">
+                    <Link
+                      to={`/update/` + user._id}
+                      type="button"
+                      className="btn btn-info"
+                    >
+                      <i className="fa-solid fa-pen-to-square"></i>
+                    </Link>
+                    <button
+                      onClick={() => deleteUser(user._id)}
+                      type="button"
+                      className="btn btn-danger"
+                    >
+                      <i className="fa-solid fa-trash"></i>
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
